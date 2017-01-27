e21d2dd7ed5d2fc5d2424cd897c7f247312087ad
shikeyue <xinluqishi@126.com>
2017-01-27 16:56:27 +0800

    defined the base html for myself and change the user page under the new base template, then push the error handling pages.

--- myFlaskCelery/helloWorld.py
+++ myFlaskCelery/helloWorld.py
@@ -36,5 +36,15 @@ def index_redirect():
     return redirect('http://www.example.com')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
